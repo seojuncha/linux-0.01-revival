43988976be935c72de49b750fe4ed17aa305c911
seojuncha <sjcha36@gmail.com>
2026-03-26 22:01:47 +0900

    work

--- linux-0.01/include/asm/system.h
+++ linux-0.01/include/asm/system.h
@@ -27,20 +27,20 @@ __asm__ (                                                       \
         "movw %0, %%dx\n\t"                                     \
         "movl %%eax, %1\n\t"                                    \
         "movl %%edx, %2"                                        \
-        :                                                       \
-        : "i"((short) (0x8000 + (dpl << 13) + (type << 8))),    \
-          "o"(*((char *)(gate_addr))),                          \
-          "o"(*(4 + (char *)(gate_addr))),                      \
-          "d"((char *)(addr)), "a"(0x00080000)                  \
+        :: "i"((short) (0x8000 + (dpl << 13) + (type << 8))),   \
+           "o"(*((char *)(gate_addr))),                         \
+           "o"(*(4 + (char *)(gate_addr))),                     \
+           "d"((char *)(addr)),                                 \
+           "a"(0x00080000)                                      \
 )
 
-#define set_intr_gate(n,addr) \
+#define set_intr_gate(n, addr) \
         _set_gate(&idt[n], 14, 0, addr)
 
-#define set_trap_gate(n,addr) \
+#define set_trap_gate(n, addr) \
         _set_gate(&idt[n], 15, 0, addr)
 
-#define set_system_gate(n,addr) \
+#define set_system_gate(n, addr) \
         _set_gate(&idt[n], 15, 3, addr)
 
 #define _set_seg_desc(gate_addr, type, dpl, base, limit) {       \
@@ -60,13 +60,13 @@ __asm__ (                                                        \
         "movw %%ax, %2\n\t"                                      \
         "rorl $16, %%eax\n\t"                                    \
         "movb %%al, %3\n\t"                                      \
-        "movb $"type", %4\n\t"                                 \
+        "movb $"type", %4\n\t"                                   \
         "movb $0x00, %5\n\t"                                     \
         "movb %%ah, %6\n\t"                                      \
         "rorl $16, %%eax"                                        \
-        :                                                        \
-        :"a"(addr), "m"(*(n)), "m"(*(n + 2)), "m"(*(n + 4)),     \
-         "m"(*(n + 5)), "m"(*(n + 6)), "m"(*(n + 7))             \
+        :: "a"(addr),                                            \
+           "m"(*(n)), "m"(*(n + 2)), "m"(*(n + 4)),              \
+           "m"(*(n + 5)), "m"(*(n + 6)), "m"(*(n + 7))           \
 )
 
 #define set_tss_desc(n,addr) \
